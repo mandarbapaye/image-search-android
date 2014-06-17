509abc9a40d05998606943d566adea5eb95dcc4f
Mandar Bapaye <mandarbapaye@yahoo.com>
2014-06-16 23:55:51 -0700

    share image

--- src/com/mb/image_search/FullImageActivity.java
+++ src/com/mb/image_search/FullImageActivity.java
@@ -1,8 +1,23 @@
 package com.mb.image_search;
 
 import static com.mb.image_search.Constants.IMAGE_URL_PARAM;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.loopj.android.image.SmartImageView;
 
@@ -19,8 +34,56 @@ public class FullImageActivity extends Activity {
 		String fullImageUrl = getIntent().getStringExtra(IMAGE_URL_PARAM);
 		sivFullImage.setImageUrl(fullImageUrl);
 	}
-
+	
 	private void setupViews() {
 		sivFullImage = (SmartImageView) findViewById(R.id.sivFullImage);
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.full_img_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	public void onShareClicked(MenuItem menuItem) {
+//		ImageView ivImage = (ImageView) findViewById(R.id.ivResult);
+	    // Get access to the URI for the bitmap
+	    Uri bmpUri = getLocalBitmapUri(sivFullImage);
+	    if (bmpUri != null) {
+	        // Construct a ShareIntent with link to image
+	        Intent shareIntent = new Intent();
+	        shareIntent.setAction(Intent.ACTION_SEND);
+	        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+	        shareIntent.setType("image/*");
+	        // Launch sharing dialog for image
+	        startActivity(Intent.createChooser(shareIntent, "Share Image"));	
+	    } else {
+	        Toast.makeText(this, "cant share", Toast.LENGTH_SHORT).show();
+	    }
+	}
+	
+	public Uri getLocalBitmapUri(ImageView imageView) {
+	    // Extract Bitmap from ImageView drawable
+	    Drawable drawable = imageView.getDrawable();
+	    Bitmap bmp = null;
+	    if (drawable instanceof BitmapDrawable){
+	       bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+	    } else {
+	       return null;
+	    }
+	    // Store image to default external storage directory
+	    Uri bmpUri = null;
+	    try {
+	        File file =  new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+	        file.getParentFile().mkdirs();
+	        FileOutputStream out = new FileOutputStream(file);
+	        bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+	        out.close();
+	        bmpUri = Uri.fromFile(file);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    return bmpUri;
+	}
+
 }
